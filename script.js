2b4216228cfd817c53a4b24c1507280434a53954
martinnovak22 <99553117+martinnovak22@users.noreply.github.com>
2022-02-20 17:08:29 +0100

    some changes to make it work on google chrome

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+//takes all elements with class "question-container"
 const questions = document.querySelectorAll(".question-container")
 
+
+//give each of that element a click function
 questions.forEach(item => {
     item.addEventListener("click", function(){
         item.classList.toggle("active");
